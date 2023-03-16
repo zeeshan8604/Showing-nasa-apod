ca150e5f4860eb144cb5b5e01b8b3fc2e12ceb81
Zeeshan Ahmed <zeeshanahmed860443@gmail.com>
2023-03-16 20:40:44 +0530

    initial

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getAPOD(data){
     $(document.createElement('img')).attr('src', data.url).appendTo('#img-container');
+    var title=data.title;
+    $('#title').text(title);
+    var date=data.date;
+    $('#datep').text(date);
+    $('img').width(700);
+    $('#img').height(700);
 }
 
 
